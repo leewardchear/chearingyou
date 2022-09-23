9c689a4050eeb1cfba35008dda1533212836cbe5
unknown <felix.ing86@gmail.com>
2022-09-23 15:09:13 -0400

    calendar added

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,30 +1,34 @@
 import React from "react";
 import { View, Button, StyleSheet } from "react-native";
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 
 function CalendarScreen() {
   return (
     <View>
       <Calendar
-        // Collection of dates that have to be colored in a special way. Default = {}
+        markingType={"custom"}
         markedDates={{
-          "2012-05-20": { textColor: "green" },
-          "2012-05-22": { startingDay: true, color: "green" },
-          "2012-05-23": {
+          "2022-09-20": { textColor: "green" },
+          "2022-09-22": { selected: true, color: "red" },
+          "2022-09-23": {
+            customStyles: {
+                container: {
+                    backgroundColor: 'green',
+                    borderRadius: 5,
+                    
+                  },
+            },
             selected: true,
-            endingDay: true,
             color: "green",
             textColor: "gray",
           },
-          "2012-05-04": {
+          "2022-09-04": {
             disabled: true,
             startingDay: true,
             color: "green",
-            endingDay: true,
+           
           },
         }}
-        // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
-        markingType={"period"}
       />
     </View>
   );
